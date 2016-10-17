80339acdd8c6b0642e515cf1a71b1482bf24dea6
rob204 <rob18x4@gmail.com>
2016-10-18 01:58:33 +0200

    Persistent green pixels.

--- Source/assets/progress/progress.js
+++ Source/assets/progress/progress.js
@@ -1,11 +1,53 @@
-document.addEventListener('click', function(e) {
-	var states = ['unset', 'one', 'two', 'three', 'all'];
-	var classes = e.target.classList;
-	states.some(function(state, i) {
-		if (classes.contains(state)) {
-			classes.remove(state);
-			classes.add(states[(i + 1) % states.length]);
-			return true;
-		}
+var states = ['unset', 'one', 'two', 'three', 'all'];
+var progress;
+var nodes;
+
+document.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+	nodes = [].slice.call(document.querySelectorAll(states.map(function(state) {
+		return '#daygrid .' + state;
+	}).join(',')));
+	nodes.forEach(function(node) {
+		node.addEventListener('click', click, false);
 	});
-}, false);
+	load();
+}
+
+function click(e) {
+	var node = e.currentTarget;
+	var index = nodes.indexOf(node);
+	node.classList.remove(states[progress[index]]);
+	progress[index] = (progress[index] + 1) % states.length;
+	node.classList.add(states[progress[index]]);
+	save();
+}
+
+function load() {
+	progress = null;
+	try {
+		progress = JSON.parse(localStorage.getItem('progressStates'));
+	} catch (e) {}
+	if (!(progress instanceof Array)) {
+		progress = nodes.map(function(node) {
+			for (var i = 0; i < states.length; ++i) {
+				if (node.classList.contains(states[i])) {
+					return i;
+				}
+			}
+		});
+	} else {
+		display();
+	}
+}
+
+function save() {
+	localStorage.setItem('progressStates', JSON.stringify(progress));
+}
+
+function display() {
+	nodes.forEach(function(node, i) {
+		node.classList.remove.apply(node.classList, states);
+		node.classList.add(states[progress[i]]);
+	});
+}
