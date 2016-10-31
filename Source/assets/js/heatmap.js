58fce9b0c0397a418b35d0b998510bdc12585860
rob204 <rob18x4@gmail.com>
2016-10-31 15:53:26 +0100

    Fix heatmap to display all events in the shown months.

--- Source/assets/js/heatmap.js
+++ Source/assets/js/heatmap.js
@@ -1,47 +1,60 @@
-var data = {}, start = new Date(), end = new Date();
+(function() {
+	var data = {}, today, currentMonth, start, end;
 
+	today = new Date();
+	today.setHours(0);
+	today.setMinutes(0);
+	today.setSeconds(0);
 
-start.setMonth(start.getMonth() - 3);
-end.setMonth(end.getMonth() + 3);
+	currentMonth = new Date(today.getTime());
+	currentMonth.setDate(1);
 
-stats.getEvents(['cummed', 'milked', 'ruined'], start.getTime(), end.getTime()).forEach(function(event) {
-	// Shift event time to make orgasms between 12am and 2am appear on the previous day.
-	data[Math.floor((event.time - startOfDay) / 1000)] = 1;
-});
+	start = new Date(currentMonth.getTime());
+	start.setMonth(start.getMonth() - 4);
 
-stats.listen(['cummed', 'milked', 'ruined'], start.getTime(), end.getTime(), function(events) {
-	var data = {};
-	events.forEach(function(event) {
+	end = new Date(currentMonth.getTime());
+	end.setMonth(end.getMonth() + 2);
+	end.setSeconds(-1);
+
+	stats.getEvents(['cummed', 'milked', 'ruined'], start.getTime(), end.getTime()).forEach(function(event) {
+		// Shift event time to make orgasms between 12am and 2am appear on the previous day.
 		data[Math.floor((event.time - startOfDay) / 1000)] = 1;
 	});
-	cal.update(data, false, cal.APPEND_ON_UPDATE);
-});
 
-var cal = new CalHeatMap();  
-cal.init({
-	domain: "month",
-	domainLabelFormat: "%b '%y",
-	subDomainDateFormat: "%m/%d/%Y",
-	subDomain: "day",
-	subDomainTitleFormat: {empty: "{date}", filled: "{count} {name}\n{date}"},
-	cellSize: 18,
-	data: data,
-	subDomainTextFormat: "%d",
-	range: 6,
-	tooltip: true,
-	weekStartOnMonday: false,
-	displayLegend: true,
-	legendVerticalPosition: "center",
-	legendHorizontalPosition: "right",
-	legendOrientation: "vertical",
-	legendMargin: [0, 10, 0, 0],
-	legend: [1, 2, 3, 4, 5],
-	itemName: ["Orgasm", "Orgasms"],
-	domainMargin: 10,
-	animationDuration: 800,
-	domainDynamicDimension: true,
-	start: start,
-	end: end,
-	//maxDate: new Date(2017, 1),
-	highlight: ["now", new Date(2017, 0, 18)],
-});  
+	stats.listen(['cummed', 'milked', 'ruined'], start.getTime(), end.getTime(), function(events) {
+		var data = {};
+		events.forEach(function(event) {
+			data[Math.floor((event.time - startOfDay) / 1000)] = 1;
+		});
+		cal.update(data, false, cal.APPEND_ON_UPDATE);
+	});
+
+	var cal = new CalHeatMap();  
+	cal.init({
+		domain: "month",
+		domainLabelFormat: "%b '%y",
+		subDomainDateFormat: "%m/%d/%Y",
+		subDomain: "day",
+		subDomainTitleFormat: {empty: "{date}", filled: "{count} {name}\n{date}"},
+		cellSize: 18,
+		data: data,
+		subDomainTextFormat: "%d",
+		range: 6,
+		tooltip: true,
+		weekStartOnMonday: false,
+		displayLegend: true,
+		legendVerticalPosition: "center",
+		legendHorizontalPosition: "right",
+		legendOrientation: "vertical",
+		legendMargin: [0, 10, 0, 0],
+		legend: [1, 2, 3, 4, 5],
+		itemName: ["Orgasm", "Orgasms"],
+		domainMargin: 10,
+		animationDuration: 800,
+		domainDynamicDimension: true,
+		start: start,
+		end: end,
+		//maxDate: new Date(2017, 1),
+		highlight: ["now"],
+	}); 
+}());
